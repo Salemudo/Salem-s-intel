9b8bf31eb72ff30c6ce70344d773e0a6f39f0aa6
Salem <111386008+Salemudo@users.noreply.github.com>
2026-03-15 09:27:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -185,4 +185,6 @@ def google_trends():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
